0f8b87b6037a937840a2a180e5f7cb96e864bb56
SSK137 <saurabhkodam@gmail.com>
2022-06-07 23:26:21 +0530

    Added Random function to check Attendance

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,3 +1,15 @@
 /*--------Employee Wage-------*/
-
 console.log("---Welcome To Employee Wage Function Using JS---");
+
+/*Check Employe is Present or Absent Using Random Function*/
+
+let Attendance=Math.floor(Math.random()*10)%2;
+console.log(Attendance);
+
+//Checking employee is present or absent
+//Present=1    Absent=0
+if(Attendance==1){
+    console.log("Employee is Present !!!");
+}else{
+    console.log("Employee is Absent !!!");
+}
