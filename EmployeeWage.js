de0046f488f86c9baa6a9f5cdb29c01791e1a3c2
SSK137 <saurabhkodam@gmail.com>
2022-06-08 08:31:03 +0530

    Added Function to check working hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -14,25 +14,26 @@ console.log(Attendance);
 //Present=1    Absent=0
 if(Attendance==1){
     console.log("Employee is Present !!!");
+    let Employee_Hours=GetWorkignHours();
+    console.log("Employee is Present for "+Employee_Hours+" Hours");
+    let Employee_Wage=Employee_Hours*WAGE_PER_HRs;
+    console.log("Employee Wage is : "+Employee_Wage);    
+    }else{
+    console.log("Employee is Absent");
+}
 
-    //Part_Time=0 Full_Time=1
+function GetWorkignHours(){
     let Attendance_Time=Math.floor(Math.random()*10)%2;
-    let Employe_Wage=0;
+    //Part_Time=0 Full_Time=1
     switch(Attendance_Time){
         case 0:
-            console.log("\nEmployee is Present for Part Time ");
-            Employe_Wage=PART_TIME_HRs*WAGE_PER_HRs;
-            console.log("\nEmployee_Wage for Part Time : "+Employe_Wage);
+            return PART_TIME_HRs;
             break;
         case 1:
-            console.log("\nEmployee is Present for Full Time ");
-            Employe_Wage=FULL_TIME_HRs*WAGE_PER_HRs;
-            console.log("\nEmployee_Wage for Full Time : "+Employe_Wage);
+            return FULL_TIME_HRs;
             break;
         default:
             console.log(" ");
             break;
-        }
-    }else{
-    console.log("Employee is Absent");
+    }
 }
